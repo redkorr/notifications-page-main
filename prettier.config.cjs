cb1fe4b7c16b28e61286655536020c800a6d9b57
redkorr <redkor910.97@gmail.com>
2023-03-20 12:21:46 +0100

    initial commit

---
+++ prettier.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  singleQuote: true,
+  arrowParens: 'always',
+  printWidth: 120,
+  tabWidth: 2,
+  trailingComma: 'none',
+  singleAttributePerLine: true,
+  bracketSpacing: true
+};
